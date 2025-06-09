a772ef3168f6b7c21199131813399ff8997b3f65
milan deori <157445208+milan-deori@users.noreply.github.com>
2025-06-09 13:19:55 +0530

    Update app.js

--- server/app.js
+++ server/app.js
@@ -18,19 +18,21 @@ allowedOrigins = [
     'http://localhost:5173',
     'https://booksfindr.somestudys.in/'           
 ]
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+// ✅ Increase payload limit
+app.use(express.json({ limit: '50mb' }));
+app.use(express.urlencoded({ limit: '50mb', extended: true }));
 app.use(cors({
   origin: function (origin, callback) {
-    if (!origin) return callback(null, true);
-    if (allowedOrigins.includes(origin)) {
-      return callback(null, true);
+    if (!origin || allowedOrigins.includes(origin)) {
+      callback(null, true);
     } else {
-      return callback(new Error('Not allowed by CORS'));
+      callback(new Error("Not allowed by CORS"));
     }
   },
   credentials: true
 }));
+
+
 
 app.use(session({
   secret: "milansecret",
